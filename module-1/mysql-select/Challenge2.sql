fba3f76d9b2858562746fd501bd0347d31b9ca48
Antonio Huerta <napoleonthevitzla@gmail.com>
2022-02-05 19:20:22 +0100

    lab-started

---
+++ module-1/mysql-select/Challenge2.sql
@@ -0,0 +1,7 @@
+SELECT a.au_id as "AUTHOR ID", a.au_lname as "Last NAME", a.au_fname as "FIRST NAME", t.title as "TITLE", p.pub_name as "PUBLISHER", COUNT (p.pub_name) as "TITLE COUNT"
+from publishers p 
+inner join titles t on t.pub_id =p.pub_id 
+INNER JOIN titleauthor t2 on t2.title_id =t.title_id 
+INNER JOIN authors a on a.au_id =t2.au_id 
+group by a.au_id
+order by "AUTHOR ID" DESC 

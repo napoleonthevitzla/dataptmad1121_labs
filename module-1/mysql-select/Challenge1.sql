9eee8958b8ea854f24f2bd231b8cf5c798010eba
Antonio Huerta <napoleonthevitzla@gmail.com>
2022-02-11 20:00:35 +0100

    lab-finished

--- module-1/mysql-select/Challenge1.sql
+++
@@ -1,5 +0,0 @@
-SELECT a.au_id as "AUTHOR ID", a.au_lname as "Last NAME", a.au_fname as "FIRST NAME", t.title as "TITLE", p.pub_name as "PUBLISHER"
-FROM titleauthor
-INNER JOIN authors as a on a.au_id = titleauthor.au_id 
-INNER JOIN titles as t on t.title_id = titleauthor.title_id
-INNER JOIN publishers as p on t.pub_id = p.pub_id 
